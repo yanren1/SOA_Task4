7e57582e6bab203e3484bad7ac197353df76ec49
Yanren <yanren@ualberta.ca>
2023-10-02 14:26:01 +0300

    First commit

---
+++ my_app/LogAndReg.py
@@ -0,0 +1,66 @@
+from flask import abort,jsonify,session
+from flask_httpauth import HTTPBasicAuth
+from my_app import usersData,app
+import secrets
+import string
+
+auth = HTTPBasicAuth()
+app.secret_key = 'your_secret_key'
+
+def generate_random_key(length):
+    characters = string.ascii_letters + string.digits
+    random_key = ''.join(secrets.choice(characters) for _ in range(length))
+    return random_key
+
+app.secret_key = generate_random_key(64)
+
+@auth.error_handler
+def unauthorized():
+    if auth.username != '':
+        return jsonify({"error": "Error Code: 403. Incorrect password or username!"}), 403
+
+    return jsonify({"error": "Error Code: 401. Unauthorized access, login required."}), 401
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in usersData and usersData[username]["PW"] == password:
+        session['user'] = usersData[username]
+        return True  # Authentication successful
+    else:
+        auth.username = username  # Store the username for use in the error handler
+        if 'user' in session:
+            session.pop('user')
+        # print("verify_password",auth.current_user())
+        return False  # Authentication failed
+
+@app.route('/login', methods=['POST'],)
+@auth.login_required
+def login():
+    return jsonify({"message": "Logged in successfully"})
+
+@app.route('/logout', methods=['POST'],)
+def logout():
+    if 'user' in session:
+        session.pop('user')
+        return jsonify({"message": "Logged out"})
+    else:
+        return jsonify({"message": "You have not logged in"})
+
+def admin_required(func):
+    def wrapper(*args, **kwargs):
+        if 'user' in session :
+            if session['user'].get('Role') == 'Admin':
+                return func(*args, **kwargs)
+            else:
+                return jsonify({"error": "Error Code: 403. Unauthorized access. Admin role required."}), 403
+        else:
+            return jsonify({"error": "Error Code: 401. Unauthorized access. login required."}), 401
+    return wrapper
+
+def login_required(func):
+    def wrapper(*args, **kwargs):
+        if 'user' in session :
+            return func(*args, **kwargs)
+        else:
+            return jsonify({"error": "Error Code: 401. Unauthorized access. login required."}), 401
+    return wrapper
